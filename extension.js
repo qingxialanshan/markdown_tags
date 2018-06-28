69e7e89982b90b0af669dc64d611a6fc9c973759
qingxialanshan <405637943@qq.com>
2018-06-28 11:22:55 +0800

    Add peek definition to support peek for markdown

--- extension.js
+++ extension.js
@@ -1,59 +1,93 @@
-//import { ViewColumn } from 'vscode';
-
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+"use strict";
+var vscode = require('vscode');
+var fs = require('fs');
+var path = require('path');
 var search_1=require('./search');
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gotodef" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let goToDef = vscode.commands.registerCommand('extension.goToDef', function () {
-        // The code you place here will be executed every time your command is executed
-        var editor=vscode.window.activeTextEditor
-        var selection=editor.selection;
-        var text=editor.document.getText(selection);
-        var document=editor.document;
-        
-        // Display a message box to the user
-        //vscode.window.showInformationMessage("Go to definition start");
-        search_1.definitionSearch(editor.document,text).then(function(location){
-            if (typeof location.fname == 'undefined'|| !location) {
-                var range = document.getWordRangeAtPosition(selection.active);
-                var message = 'unable to find the definition for ' + text;
-
-                vscode.window.showInformationMessage(message);
-                return;
-            }
-            
-            return openLocation(location);
- 
-        });
+    //console.log('Congratulations, your extension "vscode-file-peek" is now active!');
+    var config = vscode.workspace.getConfiguration('file_peek');
+    var active_languages = config.get('activeLanguages');
+    var search_file_extensions = config.get('searchFileExtensions');
+    /*
+    vscode.languages.getLanguages().then((languages: string[]) => {
+       console.log("Known languages: " + languages);
     });
-
-    context.subscriptions.push(goToDef);
+    */
+    var peek_filter = active_languages.map(function (language) {
+        return {
+            language: language,
+            scheme: 'file'
+        };
+    });
+    // Register the definition provider
+    context.subscriptions.push(vscode.languages.registerDefinitionProvider(peek_filter, new PeekFileDefinitionProvider(search_file_extensions)));
 }
 exports.activate = activate;
-
 // this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;
+/**
+ * Provide the lookup so we can peek into the files.
+ */
+var PeekFileDefinitionProvider = (function () {
+    function PeekFileDefinitionProvider(fileSearchExtensions) {
+        if (fileSearchExtensions === void 0) { fileSearchExtensions = []; }
+        this.fileSearchExtensions = [];
+        this.fileSearchExtensions = fileSearchExtensions;
+    }
+    /**
+     * Return list of potential paths to check
+     * based upon file search extensions for a potential lookup.
+     */
+    PeekFileDefinitionProvider.prototype.getPotentialPaths = function (lookupPath) {
+        var potential_paths = [lookupPath];
+        // Add on list where we just add the file extension directly
+        this.fileSearchExtensions.forEach(function (extStr) {
+            potential_paths.push(lookupPath + extStr);
+        });
+        // if we have an extension, then try replacing it.
+        var parsed_path = path.parse(lookupPath);
+        if (parsed_path.ext !== "") {
+            this.fileSearchExtensions.forEach(function (extStr) {
+                var new_path = path.format({
+                    base: parsed_path.name + extStr,
+                    dir: parsed_path.dir,
+                    ext: extStr,
+                    name: parsed_path.name,
+                    root: parsed_path.root
+                });
+                potential_paths.push(new_path);
+            });
+        }
+        return potential_paths;
+    };
+    PeekFileDefinitionProvider.prototype.provideDefinition = function (document, position, token) {
+        // todo: make this method operate async
+        var working_dir = path.dirname(document.fileName);
+        var word = document.getText(document.getWordRangeAtPosition(position));
+        var line = document.lineAt(position);
+        console.log('====== peek-file definition lookup ===========');
+        console.log('word: ' + word);
+        console.log('line: ' + line.text);
 
-function openLocation(location){
-    vscode.workspace.openTextDocument(location.fname).then(function(doc){
-        return vscode.window.showTextDocument(doc).then(function(editor){
-            var range=editor.document.lineAt(location.line_num-1).range;
-            editor.selection=new vscode.Selection(range.start,range.end);
-            return editor.revealRange(range);
-        })
-    })
-}
+        var module = path.join(require.resolve('nak'), "../../bin/nak");
+        var pattern = "::uuid::"+word;
+        var cmd = "node "+module+" --ackmate -G \"*"+path.extname(document.fileName)+"\" -d \"*node_modules*\" \""+pattern+"\" "+path.join(document.fileName,"../");
+
+        console.log(cmd);
+
+        return search_1.definitionSearch(document, word).then(function(location){
+            console.log(location);
+            return new vscode.Location(vscode.Uri.file(location.fname),new vscode.Position(parseInt(location.line_num),0));
+            //return new vscode.Location(vscode.Uri.file(path.join(working_dir,"spec.md")),new vscode.Position(parseInt(location.line_num),0));
+        });
+        //return null;
+    };
+    return PeekFileDefinitionProvider;
+}());
+//# sourceMappingURL=extension.js.map
